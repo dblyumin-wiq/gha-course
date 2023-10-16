0e8a0d8ca3373b93325565eb729ba5b19d1caca0
Dimitri Blyumin <dblyumin@woolworths.com.au>
2023-10-17 09:08:10 +1100

    add S3 deployment

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,17 @@
 const core = require('@actions/core')
-const github = require('@actions/core')
+// const github = require('@actions/core')  // useful utils for API calls and GitHub context access
 const exec = require('@actions/core')
 
 function run() {
+    // get inputs
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+
+    // upload files, using bundled AWS CLI
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
     core.notice('Hello from the custom JS action!');
 }
 
